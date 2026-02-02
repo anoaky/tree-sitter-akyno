f1e9f502d5de20e9568066825f78efd475a2f35e
Anokhi S. <17suresa@gmail.com>
2026-02-02 12:53:46 -0500

    feat: initial grammar implementation

--- grammar.js
+++ grammar.js
@@ -10,8 +10,233 @@
 export default grammar({
   name: "akyno",
 
+  supertypes: $ => [
+    $.literal,
+    $.type,
+    $.expression,
+    $.statement,
+  ],
+
+  extras: $ => [
+    /\s/,
+    $.comment,
+  ],
+
   rules: {
-    // TODO: add the actual grammar rules
-    source_file: $ => "hello"
+    source_file: $ => repeat(choice(
+      $.static_var,
+      $.fn_decl,
+      $.fn_defn,
+    )),
+    comment: $ => token(choice(seq('//', /.*/), seq('/*', /[^*]*\*+([^/*][^*]*\*+)*/, '/'))),
+    ident: $ => /([a-zA-Z][_a-zA-Z0-9]*|[_][_a-zA-Z0-9]+)/,
+    literal: $ => choice($.char, $.int, $.string),
+    char: $ => /\'([a-zA-Z0-9\\x20\\x21\\x23-\\x26\\x28-\\x2F\\x3A-\\x40\\x5B\\x5D-\\x60\\x7B-\\x7E\" ]|\\([\"\'ntr]|x[0-9]{2}))\'/,
+    int: $ => /[0-9]+/,
+    string: $ => /\"([a-zA-Z0-9\\x20\\x21\\x23-\\x26\\x28-\\x2F\\x3A-\\x40\\x5B\\x5D-\\x60\\x7B-\\x7E\' ]|\\([\"\'ntr]|x[0-9]{2}))*\"/,
+    // Top-level items
+    static_var: $ => seq(
+      'static',
+      field("name", $.ident),
+      optional(field("type", seq(':', $.type))),
+      optional(field("value", seq('=', $.literal))),
+      ';',
+    ),
+    fn_param: $ => seq(
+      field('name', $.ident),
+      ':',
+      field('type', $.type),
+    ),
+    _fn_sig: $ => seq(
+      field('name', $.ident),
+      '(',
+      field('params', optional( 
+        seq(
+          $.fn_param,
+          repeat(
+            seq(
+              ',',
+              $.fn_param,
+            )
+          )
+        )
+      )),
+      ')',
+      ':',
+      field('type', $.type),
+    ),
+    fn_decl: $ => seq(
+      $._fn_sig,
+      ';',
+    ),
+    fn_defn: $ => seq(
+      $._fn_sig,
+      field('block', $.block),
+    ),
+    // Types
+    type: $ => choice(
+      $.pointer_type,
+      $.array_type,
+      $.paren_type,
+      $.base_type,
+    ),
+    paren_type: $ => prec(18, seq('(', $.type, ')')),
+    array_type: $ => prec.right(17, seq(
+      $.type,
+      repeat1(
+        seq('[',
+            $.int,
+            ']'
+        )
+      )
+    )),
+    pointer_type: $ => prec.right(15, seq('&', $.type)),
+    base_type: $ => prec(20, choice(
+      'int',
+      'char',
+      'void',
+      seq('struct', $.ident),
+      '_',
+    )),
+    // Statements
+    statement: $ => choice(
+      $.block,
+      $.local_var,
+      $.while,
+      $.if,
+      $.return,
+      $.break,
+      $.continue,
+    ),
+    block: $ => seq(
+      '{',
+      repeat($.statement),
+      '}',
+    ),
+    local_var: $ => choice(
+      seq(
+        'let',
+        field('name', $.ident),
+        ':',
+        field('type', $.type),
+        optional(seq('=', field('expr', $.expression))),
+        ';'
+      ),
+      seq(
+        'let',
+        field('name', $.ident),
+        ':=',
+        field('expr', $.expression),
+        ';',
+      ),
+    ),
+    while: $ => seq(
+      'while',
+      '(',
+      field('expr', $.expression),
+      ')',
+      field('stmt', $.statement),
+    ),
+    if: $ => prec.right(1, seq(
+      'if',
+      '(',
+      field('expr', $.expression),
+      ')',
+      field('then', $.statement),
+      optional(
+        seq(
+          'else',
+          field('else', $.statement),
+        )
+      )
+    )),
+    return: $ => seq(
+      'return',
+      optional(field('expr', $.expression)),
+      ';',
+    ),
+    break: $ => seq(
+      'break',
+      ';',
+    ),
+    continue: $ => seq(
+      'continue',
+      ';',
+    ),
+    // Expressions
+    expression: $ => choice(
+      $.literal,
+      $.ident,
+      $.assignment,
+      $.logor,
+      $.logand,
+      $.comparison,
+      $.arithmetic_lower,
+      $.arithmetic_higher,
+      $.unary_expression,
+      $.array_access,
+      $.field_access,
+      $.call_fn,
+      $.paren_expr,
+    ),
+    assignment: $ => prec.right(1, seq(
+      field('left', $.expression),
+      '=',
+      field('right', $.expression),
+    )),
+    logor: $ => prec.left(3, seq($.expression, '||', $.expression)),
+    logand: $ => prec.left(5, seq($.expression, '&&', $.expression)),
+    eqs: $ => prec.left(7, choice(
+      seq($.expression, '==', $.expression),
+      seq($.expression, '!=', $.expression),
+    )),
+    comparison: $ => prec.left(9, choice(
+      seq($.expression, '<', $.expression),
+      seq($.expression, '>', $.expression),
+      seq($.expression, '<=', $.expression),
+      seq($.expression, '>=', $.expression),
+    )),
+    arithmetic_lower: $ => prec.left(11, choice(
+      seq($.expression, '+', $.expression),
+      seq($.expression, '-', $.expression),
+    )),
+    arithmetic_higher: $ => prec.left(13, choice(
+      seq($.expression, '*', $.expression),
+      seq($.expression, '/', $.expression),
+      seq($.expression, '%', $.expression),
+    )),
+    unary_expression: $ => prec.right(15, choice(
+      seq('+', $.expression),
+      seq('-', $.expression),
+      seq('&', $.expression),
+      seq('*', $.expression),
+    )),
+    array_access: $ => prec.right(17, seq(
+      field('arr', $.expression),
+      '[',
+      field('index', $.expression),
+      ']',
+    )),
+    field_access: $ => prec.right(17, seq(
+      field('struct', $.expression),
+      '.',
+      field('field', $.ident),
+    )),
+    call_fn: $ => prec.right(17, seq(
+      field('fn', $.ident),
+      '(',
+      field('args', optional(
+        seq(
+          $.expression,
+          optional(
+            seq(
+              ',',
+              $.expression,
+            )
+          )
+        )
+      )),
+    )),
+    paren_expr: $ => prec(19, seq('(', $.expression, ')')),
   }
 });
---
+++ queries/highlights.scm
@@ -0,0 +1,16 @@
+"let" @keyword
+"if" @keyword
+"else" @keyword
+"while" @keyword
+"return" @keyword
+"continue" @keyword
+"break" @keyword
+
+(comment) @comment
+
+(int) @constant.numeric.integer
+(char) @constant.char
+(string) @string
+
+(type) @type
+(type/base_type) @type.builtin
